b67298c60910917aa954358bca81ecd860485908
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-03 22:25:35 +0300

    Borrowing

--- chapter-04/src/main.rs
+++ chapter-04/src/main.rs
@@ -1,4 +1,5 @@
-// https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#variables-and-data-interacting-with-move
+// https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html
+// https://doc.rust-lang.org/book/ch04-02-references-and-borrowing.html
 fn main() {
     {
         let mut s = String::from("hello"); // Creating a String from a string literal using the from function
@@ -64,8 +65,47 @@ fn main() {
 
     {
         let s1 = String::from("hello");
-        let (s2, len) = calculate_length(s1);
+        let (s2, len) = calculate_length_noref(s1);
         println!("The length of '{}' is {}.", s2, len);
+        println!("The length of '{}' is {}.", s2, calculate_length_ref(&s2));
+    }
+
+    {
+        // Just as variables are immutable by default, so are references. We’re not allowed to modify something we have a reference to.
+        let s = String::from("hello");
+
+        fn change(some_string: &String) {
+            //<------------------------------->
+            // some_string.push_str(", world"); // error
+            println!("{}", some_string)
+        }
+        change(&s);
+    }
+
+    {
+        // We can fix the code to allow us to modify a borrowed value with just a few small tweaks that use, instead, a mutable reference:
+        let mut s = String::from("hello");
+    
+        fn change(some_string: &mut String) {
+            some_string.push_str(", world"); // no error
+        }
+        change(&mut s);
+        println!("{}", s);
+    }
+    
+    {
+        let mut s = String::from("hello");
+
+        let r1 = &s; // no problem
+        let r2 = &s; // no problem
+        println!("{} and {}", r1, r2);
+        // variables r1 and r2 will not be used after this point
+    
+        let r3 = &mut s; // no problem
+        // <------------------------------------------------------------>
+        // We can't use 2 diffirent mutable refereneces at the same time.
+        // let r4 = &mut s; // error
+        println!("{}", r3);
     }
     
 }
@@ -92,8 +132,12 @@ fn takes_and_gives_back(a_string: String) -> String {
     a_string  // a_string is returned and moves out to the calling function
 }
 
-fn calculate_length(s: String) -> (String, usize) {
+fn calculate_length_noref(s: String) -> (String, usize) {
     let length = s.len(); // len() returns the length of a String
 
     (s, length)
-}
+} // s and length returned and moves out to the calling function
+
+fn calculate_length_ref(s: &String) -> usize {
+    s.len()
+} // Here, s goes out of scope. But because it does not have ownership of what it refers to, it is not dropped

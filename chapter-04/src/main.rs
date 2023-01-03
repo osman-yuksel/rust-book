2a20cb75a77f630b56febe30dcc8de795033c388
Osman Yüksel <35294947+osman-yuksel@users.noreply.github.com>
2023-01-03 22:04:35 +0300

    Update main.rs

--- chapter-04/src/main.rs
+++ chapter-04/src/main.rs
@@ -1,3 +1,4 @@
+// https://doc.rust-lang.org/book/ch04-01-what-is-ownership.html#variables-and-data-interacting-with-move
 fn main() {
     {
         let mut s = String::from("hello"); // Creating a String from a string literal using the from function
@@ -19,7 +20,7 @@ fn main() {
         let s3 = s2.clone(); 
         
         //<----------------------------------->
-        //println!("{}, world!", s1); // error
+        // println!("{}, world!", s1); // error
         println!("{}, world!", s2);
         println!("{}, world!", s3); // no error
     }
@@ -40,22 +41,47 @@ fn main() {
         // s's value moves into the function and so is no longer valid here
         takes_ownership(s);
         //<------------------------>
-        //print!("{}", s); // error
+        // print!("{}", s); // error
 
 
         // x comes into scope
-        let x = 5;
+        let x = 15;
         // x would move into the function, but i32 is Copy, so it's okay to still use x afterward
         makes_copy(x);
-        print!("{}", x); // no error
+        println!("{}", x); // no error
+    }
+
+    {
+        let s1 = gives_ownership(); 
+        let s2 = String::from("hello"); 
+        let s3 = takes_and_gives_back(s2);
+        
+        println!("{}", s1); 
+        //<------------------------->
+        // print!("{}", s2); // error 
+        println!("{}", s3); // new owner of s2
     }
     
 }
 
-fn takes_ownership(some_string: String) { // some_string comes into scope
+// some_string comes into scope
+fn takes_ownership(some_string: String) { 
     println!("{}", some_string);
 } // Here, some_string goes out of scope and `drop` is called. The backing memory is freed.
 
-fn makes_copy(some_integer: i32) { // some_integer comes into scope
+// some_integer comes into scope
+fn makes_copy(some_integer: i32) { 
     println!("{}", some_integer);
 } // Here, some_integer goes out of scope. Nothing special happens.
+
+// gives_ownership will move its return value into the function that calls it
+fn gives_ownership() -> String { 
+    let some_string = String::from("yours"); // some_string comes into scope
+    some_string // some_string is returned and moves out to the calling function
+}
+
+// This function takes a String and returns one
+// a_string comes into scope
+fn takes_and_gives_back(a_string: String) -> String { 
+    a_string  // a_string is returned and moves out to the calling function
+}
